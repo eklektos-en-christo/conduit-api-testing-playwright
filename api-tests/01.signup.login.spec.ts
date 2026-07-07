9766b9823037aa41c212797a7fae67819a5269b1
eklektos-en-christo <anu518195@gmail.com>
2026-07-07 22:36:01 +0530

    update and refactor

---
+++ api-tests/01.signup.login.spec.ts
@@ -0,0 +1,60 @@
+import { test, expect, request } from '@playwright/test'
+
+let authToken: string
+
+const baseURL = 'https://conduit-api.bondaracademy.com/api'
+const email = `test${Date.now()}@gmail.com`
+const password = `${Math.random().toString(36).substring(2, 12)}`
+const username = `${Math.random().toString(36).substring(3, 12)}`
+
+test.describe.serial('auth flow', () => {
+    test('sign up success', async ({ request }) => {
+        const signUpResponse = await request.post(`${baseURL}/users`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+                "user": {
+                    "email": email,
+                    "password": password,
+                    "username": username,
+                }
+            }
+        })
+
+        const signUpResponseJSON = await signUpResponse.json()
+        authToken = signUpResponseJSON.user.token
+
+        await expect(signUpResponse.status()).toEqual(201)
+        await expect(signUpResponseJSON.user.email).toEqual(email)
+        await expect(signUpResponseJSON.user.username).toEqual(username)
+
+        // console.log(signUpResponseJSON)
+        // console.log(authtoken)
+    })
+
+    test('login success', async ({ request }) => {
+        const loginResponse = await request.post(`${baseURL}/users/login`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${authToken}`
+            },
+            data: {
+                "user": {
+                    "email": email,
+                    "password": password
+                }
+            }
+        })
+
+        const loginResponseJSON = await loginResponse.json()
+        authToken = loginResponseJSON.user.token
+
+
+        await expect(loginResponse.status()).toEqual(200)
+        await expect(loginResponseJSON.user.email).toEqual(email)
+        await expect(loginResponseJSON.user.username).toEqual(username)
+        // console.log(loginResponseJSON)
+        // console.log(authtoken)
+    })
+})
