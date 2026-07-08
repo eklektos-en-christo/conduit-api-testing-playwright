39573ecdc306720fa70bfea6616901eaf579c900
eklektos-en-christo <anu518195@gmail.com>
2026-07-08 13:29:59 +0530

    update

--- api-tests/articles.spec.ts
+++ api-tests/articles.spec.ts
@@ -77,3 +77,66 @@ test('create article with incorrect token', async ({ request }) => {
 
     expect(articleCreateResponse.status()).toEqual(401)
 })
+
+test('create, update, and delete article', async ({ request }) => {
+    const result = await auth.getAuthToken(request)
+
+    const articleCreateResponse = await request.post(`${auth.baseURL}/articles`, {
+        headers: { 'content-type': 'application/json', 'Authorization': `Token ${result.token}` },
+        data: {
+            "article": {
+                "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "description": "Description about pros and cons of Conduit API",
+                "tagList": [
+                    "proscons"
+                ],
+                "title": "Conduit API - Pros and Cons"
+            }
+        }
+    })
+
+    let body = await articleCreateResponse.json()
+
+    expect(articleCreateResponse.status()).toEqual(201)
+    expect(body.article.title).toEqual('Conduit API - Pros and Cons')
+    expect(body.article.author.username).toEqual(result.responseBody.user.username)
+
+    let articleSlug = body.article.slug
+    console.log(articleSlug)
+
+    const updateArticleResponse = await request.put(`${auth.baseURL}/articles/${articleSlug}`, {
+        headers: { 'Content-Type': 'application/json', 'Authorization': `Token ${result.token}` },
+        data: {
+            "article": {
+                "body": "Lorem ipsum dolor sit amet, consecter.",
+                "description": "Features of v3 API",
+                "slug": articleSlug,
+                "tagList": [
+                    "updates"
+                ],
+                "title": "Conduit API v3 has been released"
+            }
+        }
+    })
+
+    body = await updateArticleResponse.json()
+    articleSlug = body.article.slug
+
+    expect(updateArticleResponse.status()).toEqual(200)
+    expect(body.article.title).toEqual('Conduit API v3 has been released')
+    expect(body.article.author.username).toEqual(result.responseBody.user.username)
+
+    console.log(body.article.slug)
+
+    const deleteArticleResponse = await request.delete(`${auth.baseURL}/articles/${articleSlug}`, {
+        headers: { 'Content-Type': 'application/json', 'Authorization': `Token ${result.token}` }
+    })
+
+    expect(deleteArticleResponse.status()).toEqual(204)
+
+    const fetchDeletedArticle = await request.get(`${auth.baseURL}/articles/${articleSlug}`, {
+        headers: { 'Content-Type': 'application/json', 'Authorization': `Token ${result.token}` }
+    })
+
+    expect(fetchDeletedArticle.status()).toEqual(404)
+})
